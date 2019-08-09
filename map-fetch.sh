8bf77ca5aeb3184409a45744057daa1d70ab921a
syzygy <21166356+mrsyzygy@users.noreply.github.com>
2019-08-09 21:18:34 +1000

    Fix for API downtime
    Don't overwrite blank data

--- map-fetch.sh
+++ map-fetch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-wget "https://xnv1.getnerva.org/api/fetchanalytics.php?limit=7" -O /tmp/wget && mv /tmp/wget /www/data/connections_7d.txt
-wget "https://xnv1.getnerva.org/api/fetchanalytics.php?limit=14" -O /tmp/wget && mv /tmp/wget /www/data/connections_14d.txt
-wget "https://xnv1.getnerva.org/api/fetchanalytics.php?limit=30" -O /tmp/wget && mv /tmp/wget /www/data/connections_30d.txt
+
+wget "https://xnv1.getnerva.org/api/fetchanalytics.php?limit=7" -O tmp
+if [ `cat tmp | wc -l` != 0 ]
+ then
+        mv tmp /var/www/map.nerva.tools/data/connections_7d.txt
+        wget "https://xnv1.getnerva.org/api/fetchanalytics.php?limit=14" -O tmp && mv tmp /var/www/map.nerva.tools/data/connections_14d.txt
+        wget "https://xnv1.getnerva.org/api/fetchanalytics.php?limit=30" -O tmp && mv tmp /var/www/map.nerva.tools/data/connections_30d.txt
+fi
+else rm tmp
